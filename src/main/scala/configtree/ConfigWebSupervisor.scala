a4da96cd051f1993ceccb2ac0185f82a6c4be20b
m-malikov <malikov.ma@phystech.edu>
2017-11-19 21:23:37 +0300

    Documented ConfigWebSupervisor

--- src/main/scala/configtree/ConfigWebSupervisor.scala
+++ src/main/scala/configtree/ConfigWebSupervisor.scala
@@ -7,7 +7,16 @@ import scala.concurrent.{Future, Promise}
 import scala.util.{Failure, Success}
 import scala.concurrent.ExecutionContext.Implicits.global
 
+/**
+  * Handles interaction between webserver and actor model
+  */
+
 object ConfigWebSupervisor {
+
+  /**
+    * Creates an string with an html form for each compoment
+    */
+
   def getHtml: Future[String] = {
     val valuesMap = for ((id, component) <- Component.components) yield
       { ((component.id, component.name), component.getValue) }
@@ -27,6 +36,12 @@ object ConfigWebSupervisor {
     }
     p.future
   }
+
+  /**
+    * Updates a compoment
+    * @param id id of a component to be changed
+    * @param value new value for a component
+    */
 
   def update(id: Int, value: String): Unit = {
     Component.components{id}.changeTo(value)
