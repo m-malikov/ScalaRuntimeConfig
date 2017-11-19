eef092d219dad2a49f6fdf1cb65339d7ecb1a38b
m-malikov <malikov.ma@phystech.edu>
2017-11-19 21:12:56 +0300

    Added name field to Component

--- src/main/scala/configtree/ConfigWebSupervisor.scala
+++ src/main/scala/configtree/ConfigWebSupervisor.scala
@@ -9,15 +9,17 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 object ConfigWebSupervisor {
   def getHtml: Future[String] = {
-    val valuesMap = for ((id, component) <- Component.components) yield { (component.id , component.getValue ) }
-    val valuesFuture = Future.sequence(valuesMap.map(entry => entry._2.map(i => (entry._1, i))))
+    val valuesMap = for ((id, component) <- Component.components) yield
+      { ((component.id, component.name), component.getValue) }
+    val valuesFuture = Future.traverse(valuesMap)
+      {case (key, futureValue) => futureValue.map(key -> _)}
     val p =  Promise[String]()
     Future {
       valuesFuture.onComplete {
         case Success(list: mutable.Iterable[_]) =>
           p.success(list.foldLeft("")((acc: String, entry) =>
             acc +
-              s"""${entry._1}</br><form action='/update?id=${entry._1}' method='post'>
+              s"""${entry._1._2}</br><form action='/update?id=${entry._1._1}' method='post'>
           <input type="text" name="config_value" value="${entry._2}"></input>
           <button type='submit'>update</button></form>"""))
         case Failure(ex) => p.failure(ex)
