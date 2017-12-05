a4abcf6b2912d328d7940ea6f09ae33b797f5c72
Alexander Valentinov <valentiay@yandex.ru>
2017-12-06 01:41:08 +0300

    Small server logging

--- src/main/scala/server/RestServer.scala
+++ src/main/scala/server/RestServer.scala
@@ -13,6 +13,7 @@ class RestServer(implicit val componentSystem: ComponentSystem,
                  implicit val system: ActorSystem,
                  implicit val materializer: ActorMaterializer) extends RestService {
   def startServer(address: String, port: Int): Future[Http.ServerBinding] = {
+    println(s"Server running on $address:$port")
     Http().bindAndHandle(route, address, port)
   }
 }
